cfb4b24aeddc983ac94e8920c56e9c0c24d9d864
Mehulp214 <114227971+Mehulp214@users.noreply.github.com>
2024-07-23 00:26:55 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
 api_id = "29953693"
 api_hash = "bc080dadf0a0e8ca544ac842bcfcf2e0"
-bot_token = "7424760524:AAG-0er3b470bzc6je84PHUiNB4d45rvy4M"
+bot_token = "7407369394:AAHn3DSq_-1kK8ov1zxByeJRhLGUr1zCzdE"
